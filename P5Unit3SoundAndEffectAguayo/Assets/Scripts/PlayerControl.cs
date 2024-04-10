6ef0390729f4542f7ee0d36f3737e178e5086750
janelle <731364@student.dusd.net>
2024-04-10 13:47:52 -0700

    created spawn manager and move left
    i made the obstacle and background move left and i made the spawn manager script

--- P5Unit3SoundAndEffectAguayo/Assets/Scripts/PlayerControl.cs
+++ P5Unit3SoundAndEffectAguayo/Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,7 @@ public class PlayerControl : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +24,10 @@ public class PlayerControl : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
